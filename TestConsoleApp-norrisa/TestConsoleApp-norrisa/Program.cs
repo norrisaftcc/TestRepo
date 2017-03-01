40f3db7a7cca7e0b949f724c2d2db38cc73e2bd4
norrisa8373 <norrisa8373@ATC111INST>
2017-03-01 17:58:20 -0500

    created project

---
+++ TestConsoleApp-norrisa/TestConsoleApp-norrisa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp_norrisa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+    }
+}
